a9852c4eb9fb97a57f6e3e23dcfacc15380afb6a
Mahmoud Enani <m.a.enani21@gmail.com>
2017-04-03 21:06:11 -0400

    Initial set up - created 'Welcome' view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,91 @@
 <!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html>
+<head>
 
-        <title>Laravel</title>
+    <title>Lunch Out!</title>
+    <meta charset="utf-8">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+      <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-            .position-ref {
-                position: relative;
-            }
+    <!-- Google Fonts -->
+    <link href='//fonts.googleapis.com/css?family=Sofia' rel='stylesheet'>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+    <link href='css/main.css' rel='stylesheet'>
 
-            .content {
-                text-align: center;
-            }
+</head>
 
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+<body>
+    <div class="container-fluid" id="top">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>LUNCH OUT!</h1>
             </div>
         </div>
-    </body>
+    <div class="row">
+        <div class="col-md-12">
+            <img id="topImage" src="{{asset('/images/out_to_lunch.jpg')}}" alt="Out to Lunch"/>
+        </div>
+    </div>
+
+    <form  method='GET' action='index.php'>
+        <div class="row">
+            <div class="col-md-12">
+            </div>
+        </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <label for="cusinieType">Cuisine:</label>
+                <select name="cuisineType" class="form-control" id="cusinieType">
+                <option value="any">Any</option>
+                <option value="american">American</option>
+                <option value="chinese">Chinese</option>
+                <option value="indian">Indian</option>
+                <option value="japanese">Japanese</option>
+                <option value="mediterranean">Mediterranean</option>
+                <option value="mexican">Mexican</option>
+            </select>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <label>Budget:</label><br>
+            <label class="radio-inline"><input type="radio" name="budget" value="high">High</label>
+            <label class="radio-inline"><input type="radio" name="budget" value="medium">Medium</label>
+            <label class="radio-inline"><input type="radio" name="budget" value="low">Low</label>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <label>Dine in/Take out?</label><br>
+            <label class="radio-inline"><input type="radio" name="optinout" value="in">Dine-in</label>
+            <label class="radio-inline"><input type="radio" name="optinout" value="out">Take-out</label>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+        <label for="usr">Maximum Wait Time (mins)</label>
+             <input type="text" class="form-control" name ="waitTime" id="usr">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <br>
+            <input type="submit" class="btn btn-primary btn-small">
+            <a href="index.php" class="btn btn-warning">Clear</a>
+        </div>
+    </div>
+    </form>
+
+  </div>
+</body>
 </html>
